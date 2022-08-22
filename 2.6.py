956028ca1b89842c4179bb54e089adaa9af6c44a
mikoh <miko.heino@metropolia.fi>
2022-08-22 15:30:25 +0300

    tehtävä 1.py

---
+++ 2.6.py
@@ -0,0 +1,14 @@
+import random
+v = random.randint(1,6)
+x = random.randint(1,6)
+y = random.randint(1,6)
+z = random.randint(1,6)
+
+print(f"Nelinumeroinen koodisi on :{v, x, y, z}")
+
+w = random.randint(0, 9)
+a = random.randint(0, 9)
+d = random.randint(0,9)
+
+print(f"Kolminumeroinen koodisi on :{w, a, d}")
+
