b47098b88f0b826c0a0fd8c46c2fab7679b25ba0
mikoh <miko.heino@metropolia.fi>
2022-08-22 14:53:02 +0300

    tehtävä 1.py

---
+++ 2.4 tehtävä.py
@@ -0,0 +1,12 @@
+luku_1 = int(input("anna kokonaisluku : "))
+luku_2 = int(input("anna kokonaisluku : "))
+luku_3 = int(input("anna kokonaisluku : "))
+
+
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1 * luku_2 * luku_3
+keskiarvo = summa / 3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
