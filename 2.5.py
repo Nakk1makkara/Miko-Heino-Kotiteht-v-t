2b6c5f4e136bb9555e3d0ce472b5291cac99b544
mikoh <miko.heino@metropolia.fi>
2022-08-25 00:40:42 +0300

    tehtävä 1.py

--- 2.5.py
+++ 2.5.py
@@ -2,11 +2,6 @@ leiviskät =float(input("Anna leiviskät : "))
 naulat = float(input("Anna naulat : "))
 luodit = float(input("Anna luodit : "))
 
+paino = (luodit*13.3 + naulat*425.6 + leiviskät*8512) /1000
 
-luodit = 13.3
-naulat = 32*luodit
-leiviskät = 20*naulat
-
-paino = (luodit + naulat + leiviskät) /1000
-
-print(f"painosi nykyisten mittojen mukaan: \n{paino:3.2f}" + "kg")
+print(f"painosi nykyisten mittojen mukaan: \n{paino:4.1f}" + "kg")
