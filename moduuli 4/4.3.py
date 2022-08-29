fe78d754ef05fecebcb21fd049800d66181ba526
mikoh <miko.heino@metropolia.fi>
2022-08-29 19:18:02 +0300

    tehtävä 1.py

---
+++ moduuli 4/4.3.py
@@ -0,0 +1,15 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
+# kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
+# Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+
+suurin = 0
+pienin = 0
+
+luku = float(input("Anna luku :  "))
+
+while luku != "g":
+    suurin =
+    pienin =
+    luku = float(input("Anna luku :  "))
+print(f"Suurin luku on : {suurin}")
+print(f"Pienin luku on : {pienin}")
