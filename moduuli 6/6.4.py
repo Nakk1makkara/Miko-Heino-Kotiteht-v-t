214846ac09cf9e4543af3444982972f427501ea1
mikoh <miko.heino@metropolia.fi>
2022-09-08 13:04:48 +0300

    tehtävä 1.py

--- moduuli 6/6.4.py
+++ moduuli 6/6.4.py
@@ -2,3 +2,11 @@
 # Ohjelma palauttaa listassa olevien lukujen summan.
 # Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
 
+def laske(luvut):
+    vastaus = 0
+    for num in range(0,len(luvut)):
+        vastaus = vastaus + luvut[num]
+    return vastaus
+lista=[1,2,3,4, 5]
+yhteenlasku = laske(lista)
+print ("vastaus on: ",yhteenlasku)
