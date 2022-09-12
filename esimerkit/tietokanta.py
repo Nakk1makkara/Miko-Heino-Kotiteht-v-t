80ee8f90d769ba5f95041550f005c45fdd9ba1f5
mikoh <miko.heino@metropolia.fi>
2022-09-12 13:32:13 +0300

    tehtävä 1.py

---
+++ esimerkit/tietokanta.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='partaveikko96',
+         autocommit=True
+         )
+
