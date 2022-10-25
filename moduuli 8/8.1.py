f4ab9aa2695668883cf4c840cb27beb57d1a5059
mikoh <miko.heino@metropolia.fi>
2022-10-25 09:48:36 +0300

    tehtävä 1.py

--- moduuli 8/8.1.py
+++ moduuli 8/8.1.py
@@ -5,7 +5,7 @@ yhteys = mysql.connector.connect(
          port= 3306,
          database='flight_game',
          user='root',
-         password='OMA SALASANA TÄNNE',
+         password='',
          autocommit=True
          )
 
