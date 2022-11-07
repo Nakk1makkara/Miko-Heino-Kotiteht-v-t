f2efbc98d0f4f310f81036db2d437bf1e5352788
mikoh <miko.heino@metropolia.fi>
2022-11-07 21:45:57 +0200

    tehtävä 1.py

--- moduuli 11/teht 2.py
+++ moduuli 11/teht 2.py
@@ -5,3 +5,52 @@
 # Se kutsuu yliluokan alustajaa kahden ensin mainitun asettamiseksi sekä asettaa oman kapasiteettinsa. Kirjoita pääohjelma,
 # jossa luot yhden sähköauton (ABC-15, 180 km/h, 52.5 kWh) ja yhden polttomoottoriauton (ACD-123, 165 km/h, 32.3 l).
 # Aseta kummallekin autolle haluamasi nopeus, käske autoja ajamaan kolmen tunnin verran ja tulosta autojen matkamittarilukemat.
+
+class Car:
+    def __init__(self, LicensePlate, MaxSpeed, CurrentSpeed, Distance):
+        self.LicensePlate = LicensePlate
+        self.MaxSpeed = MaxSpeed
+        self.CurrentSpeed = CurrentSpeed
+        self.Distance = Distance
+
+    def travel(self, time):
+        self.Distance = self.Distance + self.CurrentSpeed * time
+    def accelerate(self, speed):
+
+        self.CurrentSpeed = self.CurrentSpeed + speed
+        if self.CurrentSpeed > self.MaxSpeed :
+            self.CurrentSpeed = self.MaxSpeed
+        elif self.CurrentSpeed + speed <= 0 :
+            self.CurrentSpeed = 0
+        elif self.CurrentSpeed >= self.MaxSpeed:
+            self.CurrentSpeed = self.MaxSpeed
+    def PrintInfo (self):
+        print(f"\nAuton rekisteritunnus :", self.LicensePlate)
+        print(f"Huippunopeus:", self.MaxSpeed, "KM/H")
+        print("Tämänhetkinen nopeus:", self.CurrentSpeed, "KM/H")
+        print("Kuljettu matka :", self.Distance, "KM")
+
+class Sahkoauto(Car):
+    def __init__(self,LicensePlate, MaxSpeed, CurrentSpeed, Distance, akkukapasiteetti):
+
+        self.akkukapasiteetti = akkukapasiteetti
+        super().__init__(LicensePlate, MaxSpeed,CurrentSpeed, Distance)
+
+class Polttomoottori(Car):
+    def __init__(self,LicensePlate, MaxSpeed, CurrentSpeed, Distance ,bensatankinkoko):
+        self.bensatankinkoko = bensatankinkoko
+        super().__init__(LicensePlate, MaxSpeed, CurrentSpeed, Distance)
+
+
+Sahkoauto1 = Sahkoauto("ABC-15", 180, 100,0, 52.5)
+
+polttoauto1 = Polttomoottori("ACD-123", 165, 60, 0, 32.3)
+
+
+Sahkoauto1.travel(3)
+
+polttoauto1.travel(3)
+
+print(Sahkoauto1.Distance)
+
+print(polttoauto1.Distance)
--- moduuli 12/teht 2.py
+++ moduuli 12/teht 2.py
@@ -3,4 +3,21 @@
 # Perehdy rajapinnan dokumentaatioon riittävästi. Palveluun rekisteröityminen on tarpeen,
 # jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key). Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
 
+import requests
 
+def main():
+    api_key = '8a492fb462363a9123392be795a4fa25'
+
+
+    city = input("Enter a city name: ")
+
+    city_data = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}').json()
+
+    lat = city_data['coord']['lat']
+    lon = city_data['coord']['lon']
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    response = requests.get(url).json()
+    print(response['weather'][0]['description'], str(response['main']['temp'] - 273.15) + '°C')
+
+main()
