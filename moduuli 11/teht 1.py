8feef6c0963640eaf4dc8ad7fcad07e8380056e0
mikoh <miko.heino@metropolia.fi>
2022-11-07 20:49:34 +0200

    tehtävä 1.py

--- moduuli 11/teht 1.py
+++ moduuli 11/teht 1.py
@@ -5,4 +5,32 @@
 # Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua).
 # Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
 
+class Julkaisu:
+    def __init__(self, name):
+        self.julkaisun_nimi = name
 
+class Lehti(Julkaisu):
+    def __init__(self, toimittaja, name):
+        self.headjournalist = toimittaja
+        super().__init__(name)
+
+    def tulosta_tiedot(self):
+        print(self.julkaisun_nimi, self.headjournalist)
+
+class Kirja(Julkaisu):
+    def __init__(self, kirjailija, sivumäärä, name):
+        self.kirjailija = kirjailija
+        self.sivumäärä = sivumäärä
+        super().__init__(name)
+
+    def tulosta_tiedot(self):
+        print(self.julkaisun_nimi, self.sivumäärä, self.kirjailija)
+
+AkuAnkka = Lehti("AkiHyyppä", "aku_ankka")
+
+hytti6 = Kirja("Rosa Liksom", 200, "Hytti6")
+
+
+AkuAnkka.tulosta_tiedot()
+
+hytti6.tulosta_tiedot()
--- moduuli 12/teht 1.py
+++ moduuli 12/teht 1.py
@@ -1,2 +1,12 @@
 #Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle.
 # Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/. Käyttäjälle on näytettävä pelkkä vitsin teksti.
+
+import requests
+
+haku = "https://api.chucknorris.io/jokes/random"
+
+vastaus = requests.get(haku).json()
+
+
+print(vastaus["value"])
+
