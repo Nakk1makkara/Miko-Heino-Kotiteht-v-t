6b3a4ad82cf2645fbc3cf35d15befd1c84890357
Nakki <miko.heino4@gmail.com>
2022-11-13 11:16:04 +0200

    korjattu teht 9

--- Javascript Tehtävät/moduuli 1/teht 9.js
+++ Javascript Tehtävät/moduuli 1/teht 9.js
@@ -4,26 +4,28 @@
 // On the other hand, number 21 for example is not a prime number as it can be also be divided by numbers 3 and 7.
 // Print the result on the HTML document.
 
-let kohde = document.querySelector("#kohde")
-let wholenumber = parseInt(prompt("Give a number and ill tell if its a prime number."))
 
-let numero_lista = [9, 8, 7, 6, 5, 4, 3, 2]
-let primenumber = false
 
-if (wholenumber / wholenumber == 1 && wholenumber / 1 == wholenumber){
-    primenumber = true
+
+
+let kohde = document.querySelector('#kohde');
+
+let num = prompt("Give a number: ")
+let prime = true;
+let i;
+
+if (num >1) {
+    for (i = 2; i < num; i++) {
+        if (num % i == 0) {
+        prime = false;
+        break;
+        }
+    }
 }
 
-for (number in numero_lista)
-   if (wholenumber % number == 0){
-       primenumber = false
-       }
-
-if (primenumber == true){
-    kohde.innerHTML = "The number is a prime number"
+if (prime == true){
+    kohde.innerHTML = ("Selected: " + num + ". Number is a prime number")
 }
-
-else {
-    kohde.innerHTML = "The number is not a prime number"
+else{
+    kohde.innerHTML = ("Selected: " + num + ". Number is not a prime number")
 }
-
