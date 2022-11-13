0ffbcd7ea6e6225bd5aa9611f161fb68ba994550
mikoh <miko.heino@metropolia.fi>
2022-11-13 11:11:01 +0200

    tehtävä 1.py

--- Javascript Tehtävät/moduuli 1/teht 7.js
+++ Javascript Tehtävät/moduuli 1/teht 7.js
@@ -1,2 +1,5 @@
 #! Write a program that asks the user for the number of dice rolls.
 // The program throws all the dice once and prints the sum of the numbers. (2p)
+
+let kohde = document.querySelector("#kohde")
+
--- Javascript Tehtävät/moduuli 1/teht 9.js
+++ Javascript Tehtävät/moduuli 1/teht 9.js
@@ -4,3 +4,26 @@
 // On the other hand, number 21 for example is not a prime number as it can be also be divided by numbers 3 and 7.
 // Print the result on the HTML document.
 
+let kohde = document.querySelector("#kohde")
+let wholenumber = parseInt(prompt("Give a number and ill tell if its a prime number."))
+
+let numero_lista = [9, 8, 7, 6, 5, 4, 3, 2]
+let primenumber = false
+
+if (wholenumber / wholenumber == 1 && wholenumber / 1 == wholenumber){
+    primenumber = true
+}
+
+for (number in numero_lista)
+   if (wholenumber % number == 0){
+       primenumber = false
+       }
+
+if (primenumber == true){
+    kohde.innerHTML = "The number is a prime number"
+}
+
+else {
+    kohde.innerHTML = "The number is not a prime number"
+}
+
